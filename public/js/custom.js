fff15fdf818c9b44d248c778a4a71a8a2d4ff1b7
winner00kambale <vainqueurkambale@gmail.com>
2025-06-26 21:15:29 +0200

    innitial commit

--- public/js/custom.js
+++ public/js/custom.js
@@ -53,7 +53,7 @@ $(function () {
             logo = $(".navbar .logo> img");
         if (bodyScroll > 100) {
             navbar.addClass("nav-scroll");
-            logo.attr('src', 'img/logo-sm.png');
+            logo.attr('src', 'img/logo.png');
         } else {
             navbar.removeClass("nav-scroll");
             logo.attr('src', 'img/logo.png');
