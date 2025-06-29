1d029a129176301172861965d84774e423018d5a
winner00kambale <vainqueurkambale@gmail.com>
2025-06-29 08:34:49 +0200

    galley commit

--- resources/views/webssite/gallery/gallery.blade.php
+++ resources/views/webssite/gallery/gallery.blade.php
@@ -5,29 +5,6 @@
                 <div class="section-subtitle">Images</div>
                 <div class="section-title">Images Gallerie</div>
             </div>
-
-            @foreach ($gallery as $item)
-                <div class="col-md-4 gallery-item mb-4">
-                    <a href="{{ asset('storage/' . $item->image) }}" class="img-zoom d-block">
-                        <div class="gallery-box">
-                            <div class="gallery-img">
-                                <img src="{{ asset('storage/' . $item->image) }}" alt="work-img">
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    </div>
-</section>
-
-{{-- <section class="section-padding">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section-subtitle">Images</div>
-                <div class="section-title">Images Gallerie</div>
-            </div>
             <!-- 3 columns -->
             @foreach ($gallery as $item)
                 <div class="col-md-4 gallery-item">
@@ -41,4 +18,4 @@
             @endforeach
         </div>
     </div>
-</section> --}}
+</section>
