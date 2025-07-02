a6aeea620efdd5d0e01d59d8438a51083f55cd9b
winner00kambale <vainqueurkambale@gmail.com>
2025-07-02 22:08:31 +0200

    final commit

--- resources/views/webssite/layouts/nav/nav.blade.php
+++ resources/views/webssite/layouts/nav/nav.blade.php
@@ -4,10 +4,9 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container">
         <!-- Logo -->
-        {{-- <div class="logo-wrapper">
-            <a class="logo" href="{{ '/' }}"> <img src="{{ asset('img/logo.png') }}" class="logo-img"
-                    alt=""> </a>
-        </div> --}}
+        <div class="logo-wrapper">
+            <a class="logo" href="{{ '/' }}"> <img src="{{ asset('img/logo.png') }}" alt=""> </a>
+        </div>
         <!-- Button -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
             aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"> <span
