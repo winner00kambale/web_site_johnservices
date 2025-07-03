02dfcf03832ba798aa8742ec9e86d7ff95d31c0c
winner00kambale <vainqueurkambale@gmail.com>
2025-07-03 14:25:52 +0200

    final commit

--- resources/views/webssite/rooms/details/standardDetails.blade.php
+++ resources/views/webssite/rooms/details/standardDetails.blade.php
@@ -2,9 +2,12 @@
 @section('contenu')
     <header class="header slider">
         <div class="owl-carousel owl-theme">
-            <div class="text-center item bg-img" data-overlay-dark="3" data-background="img/slider/3.jpg"></div>
-            <div class="text-center item bg-img" data-overlay-dark="3" data-background="img/slider/2.jpg"></div>
-            <div class="text-center item bg-img" data-overlay-dark="3" data-background="img/slider/5.jpg"></div>
+            <div class="text-center item bg-img" data-overlay-dark="3" data-background="{{ asset('img/standard/1.jpeg') }}">
+            </div>
+            <div class="text-center item bg-img" data-overlay-dark="3" data-background="{{ asset('img/standard/2.jpeg') }}">
+            </div>
+            <div class="text-center item bg-img" data-overlay-dark="3" data-background="{{ asset('img/standard/3.jpeg') }}">
+            </div>
         </div>
         <div class="arrow bounce text-center">
             <a href="#" data-scroll-nav="1" class=""> <i class="ti-arrow-down"></i> </a>
