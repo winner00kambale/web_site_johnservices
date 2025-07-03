0e11a7586990626f6bd699fe995a4dfd8e094a67
winner00kambale <vainqueurkambale@gmail.com>
2025-07-03 11:26:24 +0200

    final commit

--- resources/views/webssite/partennaire/partennaire.blade.php
+++ resources/views/webssite/partennaire/partennaire.blade.php
@@ -1,12 +1,13 @@
 <section class="clients">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 mx-auto text-center">
                 <div class="owl-carousel owl-theme">
                     @foreach ($partenaire as $item)
                         <div class="clients-logo">
-                            <a href="{{ $item->website }}"><img src="{{ asset('storage/' . $item->image) }}"
-                                    alt=""></a>
+                            <a href="{{ $item->website }}">
+                                <img src="{{ asset('storage/' . $item->image) }}" alt="">
+                            </a>
                         </div>
                     @endforeach
                 </div>
