2840ee913dd17167e18e405faedc2352e4854abc
winner00kambale <vainqueurkambale@gmail.com>
2025-07-05 14:11:24 +0200

    final commit

--- resources/views/webssite/layouts/header/heade.blade.php
+++ resources/views/webssite/layouts/header/heade.blade.php
@@ -3,9 +3,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <title>John services Motel</title>
-    <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" />
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Barlow&amp;family=Barlow+Condensed&amp;family=Gilda+Display&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/plugins.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/image.css') }}">
